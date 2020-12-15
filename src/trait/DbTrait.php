98c987019d9a92d8e9185e83a47b90e6b5ccc624
Kerem Güneş <qeremy@gmail.com>
2020-12-15 05:51:20 +0300

    Add trait\DbTrait.

---
+++ src/trait/DbTrait.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright (c) 2015 · Kerem Güneş
+ * Apache License 2.0 <https://opensource.org/licenses/apache-2.0>
+ */
+declare(strict_types=1);
+
+namespace froq\database\trait;
+
+use froq\database\Database;
+
+/**
+ * Db Trait.
+ *
+ * Represents a trait entity that holds a read-only `$db` property and its getter method.
+ *
+ * @package froq\database\trait
+ * @object  froq\database\trait\DbTrait
+ * @author  Kerem Güneş <k-gun@mail.com>
+ * @since   5.0
+ * @internal
+ */
+trait DbTrait
+{
+    /** @var froq\database\Database */
+    protected Database $db;
+
+    /**
+     * Get db property.
+     *
+     * @return froq\database\Database|null
+     */
+    public final function db(): Database|null
+    {
+        return $this->db ?? null;
+    }
+}
