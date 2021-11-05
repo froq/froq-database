f089a97e71f111ae30e3ca03ff615c61baa4a12a
Kerem Güneş <qeremy@gmail.com>
2021-11-05 20:38:16 +0300

    entity.MetaFactory: add docs.

--- src/entity/MetaFactory.php
+++ src/entity/MetaFactory.php
@@ -9,10 +9,31 @@ namespace froq\database\entity;
 
 use froq\database\entity\{MetaException, Meta, EntityClassMeta, EntityPropertyMeta};
 
+/**
+ * Meta.
+ *
+ * Represents a factory entity that used for creating EntityClassMeta & EntityPropertyMeta.
+ *
+ * @package froq\database\entity
+ * @object  froq\database\entity\MetaFactory
+ * @author  Kerem Güneş
+ * @since   5.0
+ */
 final class MetaFactory
 {
+    /** @var array */
     private static array $cache;
 
+    /**
+     * Init a meta class.
+     *
+     * @param  int        $type
+     * @param  string     $name
+     * @param  string     $class
+     * @param  array|null $data
+     * @return froq\database\entity\Meta
+     * @throws froq\database\entity\MetaException
+     */
     public static function init(int $type, string $name, string $class, array $data = null): Meta
     {
         return self::$cache[$name] ??= match ($type) {
@@ -22,23 +43,60 @@ final class MetaFactory
         };
     }
 
+    /**
+     * Init a class meta.
+     *
+     * @param  string     $class
+     * @param  array|null $data
+     * @return froq\entity\EntityClassMeta
+     */
     public static function initEntityClassMeta(string $class, array $data = null): EntityClassMeta
     {
         return self::init(Meta::TYPE_CLASS, $class, $class, $data);
     }
+
+    /**
+     * Init a property meta.
+     *
+     * @param  string     $name
+     * @param  string     $class
+     * @param  array|null $data
+     * @return froq\entity\EntityPropertyMeta
+     */
     public static function initEntityPropertyMeta(string $name, string $class, array $data = null):  EntityPropertyMeta
     {
         return self::init(Meta::TYPE_PROPERTY, $name, $class, $data);
     }
 
+    /**
+     * Check a cache item.
+     *
+     * @param  string $name
+     * @return bool
+     */
     public static function hasCacheItem(string $name): bool
     {
         return isset(self::$cache[$name]);
     }
+
+    /**
+     * Set a cache item.
+     *
+     * @param  string                    $name
+     * @param  froq\database\entity\Meta $item
+     * @return void
+     */
     public static function setCacheItem(string $name, Meta $item): void
     {
         self::$cache[$name] = $item;
     }
+
+    /**
+     * Get a cache item.
+     *
+     * @param  string                         $name
+     * @return froq\database\entity\Meta|null $item
+     */
     public static function getCacheItem(string $name): Meta|null
     {
         return self::$cache[$name] ?? null;
